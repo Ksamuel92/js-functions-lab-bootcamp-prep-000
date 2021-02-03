768b9ada98887c1a8c4a18f40d86cc0683dda25b
Kyle Samuel <ksamuel92@gmail.com>
2021-02-03 20:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function happyHolidays () {
   return "Happy holidays!";
   }
   
-  function happyHolidaysTo (name) => {
-    
+  function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`;    
+  }
+  
+  function happyCustomHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+  }
+  
+  function holidayCountdown (holiday, days) {
+  return   `It's ${days} until ${holiday}!`;
   }
